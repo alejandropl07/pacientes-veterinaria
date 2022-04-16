76318a8254b158d0e24f5c083e4a0f4fda6c5061
alejandropl07 <aprietoleon97@gmail.com>
2022-04-16 01:15:15 -0500

    Header creado

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { Component } from "react";
 import './bootstrap.min.css';
+import Header from "./components/Header";
 
-function App() {
-  return (
-    <div>
-    
-    </div>
-  );
+class App extends Component{
+  state = {
+
+  }
+  render()  {
+    return  (
+      <div  className="container">
+        <Header 
+        titulo  = 'Administrador pacientes veterinaria'
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
