eea81a1631a04ef08a2c956f11db0ed41f9a89fc
alejandropl07 <aprietoleon97@gmail.com>
2022-04-16 20:37:08 -0500

    Cita agregada al state

--- src/App.js
+++ src/App.js
@@ -5,7 +5,14 @@ import NuevaCita from "./components/NuevaCita";
 
 class App extends Component{
   state = {
+    citas:  []
+  }
 
+  crearNuevaCita  = datos =>{
+    const citas = [...this.state.citas, datos]
+    this.setState({
+      citas
+    })
   }
   render()  {
     return  (
@@ -15,7 +22,9 @@ class App extends Component{
         />
         <div className="row">
           <div className="col-md-10 mx-auto">
-            <NuevaCita/>
+            <NuevaCita
+            crearNuevaCita  = {this.crearNuevaCita}
+            />
           </div>  
         </div>
         
--- src/components/NuevaCita.js
+++ src/components/NuevaCita.js
@@ -1,4 +1,5 @@
 import React , { Component } from "react";
+import {v4 as uuid} from "uuid";
 
 class NuevaCita extends Component{
     state = {
@@ -38,17 +39,29 @@ class NuevaCita extends Component{
             return;
         }
 
+        //Generar objeto
+        const nuevaCita = {
+            ...this.state.cita
+        }
+        nuevaCita.id    =   uuid();
+
         //Agregar cita al state
-        
+        this.props.crearNuevaCita(nuevaCita)
     }
 
     render()  {
+        //Extrae valor del state
+        const {error}   =   this.state;
       return  (
         <div  className="card   mt-5    py-5">
             <div    className="card-body">
                 <h2 className="card-title text-center mb-5">
                     Llena el formulario para crear una cita
                 </h2>
+
+                {error ?  <div className="alert alert-danger    mt-2    mb-5    text-center">
+                    Todos los campos son obligatorios
+                </div>  : null}
                 <form
                 onSubmit={this.handleSubmit}>
                     <div className="form-group row">
